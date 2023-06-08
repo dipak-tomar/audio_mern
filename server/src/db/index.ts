465a5f27cb33f126c01e83d375a5b0bdf1351830
dipak-tomar <dipakkumartomar758@gmail.com>
2023-06-08 10:05:51 +0530

    added database connection and model

---
+++ server/src/db/index.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { URI } from "@/utils/variables";
+
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("db is connected");
+  })
+  .catch((err) => {
+    console.log("db connection failed: ", err);
+  });
