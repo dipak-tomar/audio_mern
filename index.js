1f7587d246113c42eb389c8d1ccaccab96a45a18
dipak-tomar <dipakkumartomar758@gmail.com>
2023-06-08 13:33:52 +0530

    added hash method for email token

--- index.js
+++
@@ -1,1 +0,0 @@
-console.log("hi starting the project");
--- server/src/models/emailVerificationToken.ts
+++ server/src/models/emailVerificationToken.ts
@@ -1,5 +1,5 @@
 import { Model, model, ObjectId, Schema } from "mongoose";
-
+import { hash, compare } from "bcrypt";
 // interface (typescript)
 interface EmailVerificationTokenDocument {
   owner: ObjectId;
@@ -7,28 +7,52 @@ interface EmailVerificationTokenDocument {
   createdAt: Date;
 }
 
-// expire them after 1 hrs
+interface Methods {
+  compareToken(token: string): Promise<boolean>;
+}
 
-const emailVerificationTokenSchema = new Schema<EmailVerificationTokenDocument>(
-  {
-    owner: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
-    },
-    token: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      expires: 3600, // 60 min * 60 sec = 3600s
-      default: Date.now(),
-    },
+const emailVerificationTokenSchema = new Schema<
+  EmailVerificationTokenDocument,
+  {},
+  Methods
+>({
+  owner: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    expires: 3600, // 60 min * 60 sec = 3600s
+    default: Date.now(),
+  },
+});
+
+/**
+ * This is the middleware, It will be called before saving any record
+ */
+emailVerificationTokenSchema.pre("save", async function (next) {
+  // check if password is present and is modified.
+  if (this.isModified("token")) {
+    // call your hashPassword method here which will return the hashed password.
+    this.token = await hash(this.token, 10);
   }
-);
+
+  // everything is done, so let's call the next callback.
+  next();
+});
+emailVerificationTokenSchema.methods.compareToken = async function (
+  token: string
+) {
+  const result = await compare(token, this.token);
+  return result;
+};
 
 export default model(
   "EmailVerificationToken",
   emailVerificationTokenSchema
-) as Model<EmailVerificationTokenDocument>;
+) as Model<EmailVerificationTokenDocument, {}, Methods>;
